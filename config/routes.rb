f8caee13e9a0e2ba252f355db54a7825f68d2ae2
Jen McQuade <jen.k.mcquade@gmail.com>
2020-02-22 13:19:12 -0800

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
 
   resources :articles
+  resources :users, except: [:new]
+
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
 end
