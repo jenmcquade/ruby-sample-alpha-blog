fc69320794c3822e6f1782de6b20b8156bcfb106
Jen McQuade <jen.k.mcquade@gmail.com>
2020-02-22 17:49:54 -0800

    Added LiveReload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
 
+  def show
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
+
   def new
     @user = User.new
+  end
+
+  def edit
   end
 
   def create
@@ -17,12 +25,7 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
-
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = 'Your account was updated successfully'
       redirect_to articles_path
@@ -31,13 +34,13 @@ class UsersController < ApplicationController
     end
   end
 
-  def show
+  private
+
+  def set_user
     @user = User.find(params[:id])
-    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
-  private
-    def user_params
-      params.require(:user).permit(:username, :email, :password)
-    end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
