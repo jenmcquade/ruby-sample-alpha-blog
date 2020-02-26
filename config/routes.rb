18dc4526bf807b7065d557165983621fc049b0b0
Jen McQuade <jen.k.mcquade@gmail.com>
2020-02-25 18:31:53 -0800

    Add categories model and tests

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   resources :articles
   resources :users, except: [:new]
+  resources :categories, except: [:destroy]
 
   get 'signup', to: 'users#new'
   post 'users', to: 'users#create'
