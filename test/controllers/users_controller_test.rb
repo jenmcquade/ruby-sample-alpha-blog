7dd95e20bbe6881553f226378deb77fc01d600a5
Jen McQuade <jen.k.mcquade@gmail.com>
2020-02-28 18:58:20 -0800

    Added users controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create(username: 'john', email: 'john@example.com', password: 'password')
+  end
+
+  test 'should get users index' do
+    get users_path
+    assert_response :success
+  end
+
+  test 'should get new from /signup' do
+    get '/signup'
+    assert_response :success
+  end
+
+  test 'should get show' do
+    get user_path(@user)
+    assert_response :success
+  end
+
+  test 'should get /login' do
+    get '/login'
+    assert_response :success
+  end
+
+  test 'should redirect to user page after login' do
+    sign_in_as(@user, 'password')
+    follow_redirect!
+    assert session[:user_id] == @user.id
+    assert_template 'users/show'
+  end
 end
